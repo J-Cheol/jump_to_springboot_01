fe41a9c7adc51b5a4653e54013a348545b8bb678
J-Cheol <lunaris0206@naver.com>
2024-01-25 10:54:09 +0900

    start

---
+++ src/main/java/com/ll/jumb_to_springboot_01/JumbToSpringboot01Application.java
@@ -0,0 +1,13 @@
+package com.ll.jumb_to_springboot_01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JumbToSpringboot01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JumbToSpringboot01Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/ll/jumb_to_springboot_01/JumbToSpringboot01ApplicationTests.java
@@ -0,0 +1,15 @@
+package com.ll.jumb_to_springboot_01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JumbToSpringboot01ApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
