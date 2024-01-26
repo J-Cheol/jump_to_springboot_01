e2c07864129e33d0bbaec259908e5a08f1952dbf
J-Cheol <lunaris0206@naver.com>
2024-01-26 23:56:18 +0900

    2-02 : MainController add
    status = 500 오류

---
+++ src/main/java/com/ll/jumb_to_springboot_01/MainController.java
@@ -0,0 +1,12 @@
+package com.ll.jumb_to_springboot_01;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+    @GetMapping("/sbb")
+    public void index() {
+        System.out.println("index");
+    }
+}
