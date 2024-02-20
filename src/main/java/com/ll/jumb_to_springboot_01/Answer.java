472cee15ba86170acc5745b122cddb76b14900d6
전화철 <lunaris0206@naver.com>
2024-02-20 17:31:09 +0900

    2-04 : Answer Entity

---
+++ src/main/java/com/ll/jumb_to_springboot_01/Answer.java
@@ -0,0 +1,24 @@
+package com.ll.jumb_to_springboot_01;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Setter
+@Getter
+@Entity
+public class Answer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private LocalDateTime createDate;
+
+    @ManyToOne
+    private Question question;
+}
