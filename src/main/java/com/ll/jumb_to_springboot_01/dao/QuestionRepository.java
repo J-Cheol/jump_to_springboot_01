aedb364e13b210558540e5a9befda58bf1e79b33
전화철 <lunaris0206@naver.com>
2024-02-21 17:44:03 +0900

    2-05 : findBySubjectLike

--- src/main/java/com/ll/jumb_to_springboot_01/dao/QuestionRepository.java
+++ src/main/java/com/ll/jumb_to_springboot_01/dao/QuestionRepository.java
@@ -3,7 +3,10 @@ package com.ll.jumb_to_springboot_01.dao;
 import com.ll.jumb_to_springboot_01.Entity.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface QuestionRepository extends JpaRepository<Question, Integer> {
     Question findBySubject(String subject);
     Question findBySubjectAndContent(String subject, String content);
+    List<Question> findBySubjectLike(String subject);
 }
