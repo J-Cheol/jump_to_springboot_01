d36914119303d6a97700212aa29309a7b0aa827d
전화철 <lunaris0206@naver.com>
2024-02-21 17:59:22 +0900

    2-05 : Answer search

--- src/test/java/com/ll/jumb_to_springboot_01/JumbToSpringboot01ApplicationTests.java
+++ src/test/java/com/ll/jumb_to_springboot_01/JumbToSpringboot01ApplicationTests.java
@@ -1,16 +1,15 @@
 package com.ll.jumb_to_springboot_01;
 
 import com.ll.jumb_to_springboot_01.Entity.Answer;
-import com.ll.jumb_to_springboot_01.Entity.Question;
 import com.ll.jumb_to_springboot_01.dao.AnswerRepository;
 import com.ll.jumb_to_springboot_01.dao.QuestionRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.time.LocalDateTime;
 import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
@@ -22,15 +21,10 @@ class JumbToSpringboot01ApplicationTests {
     private AnswerRepository answerRepository;
     @Test
     void testJpa() {
-        Optional<Question> oq = this.questionRepository.findById(2);
-        assertTrue(oq.isPresent());
-        Question q = oq.get();
-
-        Answer a = new Answer();
-        a.setContent("네 자동으로 생성됩니다.");
-        a.setQuestion((q));
-        a.setCreateDate(LocalDateTime.now());
-        this.answerRepository.save(a);
+        Optional<Answer> oa = this.answerRepository.findById(1);
+        assertTrue(oa.isPresent());
+        Answer a = oa.get();
+        assertEquals(2, a.getQuestion().getId());
     }
 
 }
