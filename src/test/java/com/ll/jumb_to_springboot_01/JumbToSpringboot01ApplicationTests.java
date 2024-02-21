8cfb221c565e579c78ed981fdb16478858c8358d
전화철 <lunaris0206@naver.com>
2024-02-21 17:55:02 +0900

    2-05 : Answer create

--- src/test/java/com/ll/jumb_to_springboot_01/JumbToSpringboot01ApplicationTests.java
+++ src/test/java/com/ll/jumb_to_springboot_01/JumbToSpringboot01ApplicationTests.java
@@ -1,14 +1,16 @@
 package com.ll.jumb_to_springboot_01;
 
+import com.ll.jumb_to_springboot_01.Entity.Answer;
 import com.ll.jumb_to_springboot_01.Entity.Question;
+import com.ll.jumb_to_springboot_01.dao.AnswerRepository;
 import com.ll.jumb_to_springboot_01.dao.QuestionRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
@@ -16,14 +18,19 @@ class JumbToSpringboot01ApplicationTests {
     @Autowired
     private QuestionRepository questionRepository;
 
+    @Autowired
+    private AnswerRepository answerRepository;
     @Test
     void testJpa() {
-        assertEquals(2, this.questionRepository.count());
-        Optional<Question> oq = this.questionRepository.findById(1);
+        Optional<Question> oq = this.questionRepository.findById(2);
         assertTrue(oq.isPresent());
         Question q = oq.get();
-        this.questionRepository.delete(q);
-        assertEquals(1, this.questionRepository.count());
+
+        Answer a = new Answer();
+        a.setContent("네 자동으로 생성됩니다.");
+        a.setQuestion((q));
+        a.setCreateDate(LocalDateTime.now());
+        this.answerRepository.save(a);
     }
 
 }
