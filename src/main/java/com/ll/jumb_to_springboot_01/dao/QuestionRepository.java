ec6c1491bd9cee15b682fcc82d317eb5f42591d6
전화철 <lunaris0206@naver.com>
2024-02-21 17:35:35 +0900

    2-05 : findBySubject

--- src/main/java/com/ll/jumb_to_springboot_01/dao/QuestionRepository.java
+++ src/main/java/com/ll/jumb_to_springboot_01/dao/QuestionRepository.java
@@ -4,4 +4,5 @@ import com.ll.jumb_to_springboot_01.Entity.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface QuestionRepository extends JpaRepository<Question, Integer> {
+    Question findBySubject(String subject);
 }
